c8c1430099c0daa49345319b3cc0bb4e986479b2
seongsan14 <eekek@naver.com>
2015-12-16 14:56:09 +0900

    test2

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var passport = require("passport");
 var express =  require("express");
 var KakaoStrategy = require("passport-kakao").Strategy;
+var requestKakaoLogin = require('request');
 
-var appKey = "YOUR_APP_KEY";
+var appKey = "acd16274636c8807be1f6e2aa436ae52";
+var redirectUrl = "http://localhost:3000/oauth";
 
 // passport 에 Kakao Oauth 추가
 passport.use( new KakaoStrategy({
@@ -29,6 +31,28 @@ passport.deserializeUser(function(obj, done) {
 var app = express();
 app.use(passport.initialize());
 app.get("/login", passport.authenticate('kakao',{state: "myStateValue"}));
+app.post("/kakao-login", function(req, res){
+  var result;
+  var isWait = true;
+  console.log(">>kakao-login request:" + req.email + "/" + req.password);
+  requestKakaoLogin('https://kauth.kakao.com/oauth/authorize?client_id=' + appKey + '&redirect_uri=' + redirectUrl + '&response_type=code', function (error, response, body) {
+    // if (!error && response.statusCode == 200) {
+    //   console.log(body); // Print the body of response.
+    // }
+    // console.log(body);
+    // res.send("result:" + response);
+
+    console.log("<<kakao-login return:" + response);
+    result = body;
+    isWait = false;
+  });
+
+  while (isWait) {
+    require('deasync').sleep(100);
+  }
+
+  res.send(result);
+});
 app.get("/oauth", passport.authenticate('kakao'), function(req, res){
     // 로그인 시작시 state 값을 받을 수 있음
     res.send("state :" + req.query.state);
